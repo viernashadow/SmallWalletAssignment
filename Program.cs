6e81789f776940cba01d499a4d6db31958c01a06
Galaxia <viernashadow@gmail.com>
2024-05-25 15:49:19 -0400

    Base dialogue for user

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
-﻿Console.WriteLine("Hey, you're poor you only have 30$ in your small wallet.");
-Console.WriteLine("You need to buy a new phone, but you don't have enough money.");
-Console.WriteLine("you have to buy food from the really pushy expensive food truck thing though.");
-var wallet = 30;
+﻿var money = 30;
 
+Console.WriteLine($"Hey, you're poor you only have ${money} in your small wallet.");
+Console.WriteLine("YOU HAVE NO CHOICE.");
+Console.WriteLine("do you want to buy:");
+Console.WriteLine(" \"can of baked beans\" -10$, ");
+Console.WriteLine(" \"Meaty Cow Leg\" - 25$,");
+Console.WriteLine(" \"Juice\" - 7$, ");
+Console.WriteLine(" \"Sugar\" - 5$");
