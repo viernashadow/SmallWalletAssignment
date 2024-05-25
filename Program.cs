2cea48f7cb8c8c457738d68c54d452c5c92944bc
Galaxia <viernashadow@gmail.com>
2024-05-25 16:27:09 -0400

    Fixed and it works according to asssignment specifications

--- Program.cs
+++ Program.cs
@@ -9,23 +9,23 @@ while (money >= 5)
     Console.WriteLine(" \"Beans\" -10$, ");
     Console.WriteLine(" \"Juice\" - 7$, ");
     Console.WriteLine(" \"Sugar\" - 5$");
-
-    if (Console.ReadLine().ToLower() == "Beans" && money >= 10)
+    var input = Console.ReadLine().ToLower();
+    if (input == "beans" && money >= 10)
     {
         money -= 10;
         Console.WriteLine("You bought Beans");
     }
-    else if (Console.ReadLine().ToLower() == "Meat" && money >= 25)
+    else if (input == "meat" && money >= 25)
     {
         money -= 25;
         Console.WriteLine("You bought Meat");
     }
-    else if (Console.ReadLine().ToLower() == "juice" && money >= 7)
+    else if (input == "juice" && money >= 7)
     {
         money -= 7;
         Console.WriteLine("You bought a juice");
     }
-    else if (Console.ReadLine().ToLower() == "sugar" && money >= 5)
+    else if (input == "sugar" && money >= 5)
     {
         money -= 5;
         Console.WriteLine("You bought some sugar");
@@ -40,4 +40,3 @@ if (money < 5)
 {
     Console.WriteLine("You're BROKE, you can't buy Anything.");
 }
-
